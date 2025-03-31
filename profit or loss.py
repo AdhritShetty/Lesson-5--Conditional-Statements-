00fbc1c81c11585d586e4fd0bbb2e48815eea014
AdhritShetty <adhritshetty2009@gmail.com>
2025-03-31 20:51:40 +0000

    Profit or loss

---
+++ profit or loss.py
@@ -0,0 +1,7 @@
+actualprice=int(input("enter the actual cost of the product:"))
+sellingprice=int(input("enter the selling cost of the product:"))
+
+if sellingprice>actualprice:
+    print("profit")
+else:
+    print("loss")
