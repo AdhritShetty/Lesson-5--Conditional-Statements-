0a74239c1031888e0ddef4083d6b91fdc63fc176
AdhritShetty <adhritshetty2009@gmail.com>
2025-04-01 02:34:34 +0000

    Greater or less than

---
+++ Greater or Smaller.py
@@ -0,0 +1,6 @@
+firstnum=int(input("enter the first number:"))
+secondnum=int(input("enter the second number:"))
+if firstnum>secondnum:
+ print(firstnum,"is greater than",secondnum)
+else:
+    print(firstnum,"is less than",secondnum)
